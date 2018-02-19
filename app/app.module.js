feb6b132204f598bc809bfc27929a21021844bfb
dzimnitsky <dzimnitsky@dev-team.com>
2018-02-19 15:18:54 +0300

    continue learn components

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,4 @@
 "use strict";
-// import { NgModule } from "@angular/core";
-// import { BrowserModule } from "@angular/platform-browser";
-// import { RouterModule } from "@angular/router"; // модуль для маршрутизации
-// import { AppComponent } from "./app.component";
-// import { List1Component } from "./list1/list1.component" ;
-// import { List2Component } from "./list2/list2.component" 
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -12,19 +6,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// @NgModule({
-//     imports :[
-//         BrowserModule,
-//         RouterModule.forRoot([
-//             {path: "list1" , component: List1Component},
-//             {path: "list2" , component: List2Component},
-//             {path: "" , component: List1Component }
-//         ])
-//     ],
-//     declarations: [AppComponent, List1Component, List2Component],
-//     bootstrap: [AppComponent] 
-// })
-// export class AppModule { }
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var router_1 = require("@angular/router"); // модуль для маршрутизации
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,27 +1,3 @@
-// import { NgModule } from "@angular/core";
-// import { BrowserModule } from "@angular/platform-browser";
-// import { RouterModule } from "@angular/router"; // модуль для маршрутизации
-// import { AppComponent } from "./app.component";
-// import { List1Component } from "./list1/list1.component" ;
-// import { List2Component } from "./list2/list2.component" 
-
-
-
-// @NgModule({
-//     imports :[
-//         BrowserModule,
-//         RouterModule.forRoot([
-//             {path: "list1" , component: List1Component},
-//             {path: "list2" , component: List2Component},
-//             {path: "" , component: List1Component }
-//         ])
-//     ],
-//     declarations: [AppComponent, List1Component, List2Component],
-//     bootstrap: [AppComponent] 
-// })
-// export class AppModule { }
-
-
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router"; // модуль для маршрутизации
